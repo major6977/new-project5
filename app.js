0ccd03f47868f8ade29f0cd284cdad5fc741f2c5
major6977 <majordav@msn.com>
2022-10-15 11:16:09 -0700

    added swicth functions

--- app.js
+++ app.js
@@ -3,20 +3,66 @@ for (var i = 0; i < cars.length; i++) {
   document.getElementsByTagName("li")[i].innerHTML = `<p>New ${cars[i]}</p>`;
 }
 
-function date() {
-  let date = Date();
-  document.getElementById("text").innerHTML = "Today's Date Is " + date;
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    days.style.backgroundColor = "red";
+    days.style.width = "10vw";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    days.style.backgroundColor = "pink";
+    days.style.width = "10vw";
+    break;
 }
-const d = new Date();
-let dayOfTheWeek = d.getDay() + 1;
-function love() {
-  if (dayOfTheWeek == 0) {
-    dayOfTheWeek = "Sunday";
-  } else if (dayOfTheWeek) {
-    dayOfTheWeek = "Thursday" + dayOfTheWeek;
-  }
-  document.getElementById("days").innerHTML = "Today is " + dayOfTheWeek;
-}
+
+document.getElementById("days").innerHTML = "Today Is " + day;
+
+let state = "Country Chicago 2";
+document.getElementById("eat").innerText = state;
+
+const sweet = "Let them eat cake";
+document.getElementById("food").innerText = sweet;
+
+const love = "Give it to me know";
+document.getElementById("eatMe").innerText = love;
+
+let hit = "for the love on money";
+let count = 84 + 12 + 12;
+let count2 = 14;
+const total = count2 + count;
+
+document.getElementById("fart").innerHTML = `<h1>${hit}</h1> ${total}`;
+
+//   let date = Date();
+//   document.getElementById("text").innerHTML = "Today's Date Is " + date;
+// }
+// const d = new Date();
+// let dayOfTheWeek = d.getDay() + 1;
+// function love() {
+//   if (dayOfTheWeek == 0) {
+//     dayOfTheWeek = "Sunday";
+//   } else if (dayOfTheWeek) {
+//     dayOfTheWeek = "Thursday" + dayOfTheWeek;
+//   }
+//   document.getElementById("days").innerHTML = "Today is " + dayOfTheWeek;
+// }
 
 // document.body.style.backgroundColor = "red";
 
